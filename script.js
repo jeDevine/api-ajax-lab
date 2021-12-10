a0f44edba85cb115a0a8b0a983ee13a543150ff5
james <jamesedwarddevine@gmail.com>
2021-12-10 11:35:27 -0500

    made html elements to fit actual content not sorces

--- script.js
+++ script.js
@@ -8,9 +8,26 @@ fetch("https://www.reddit.com/r/aww/.json")
     let box = document.createElement("div");
     let name = data.data.children[i].data.name;
     let titler = data.data.children[i].data.title;
-    let link = data.data.children[i].data.permalink;
+    let permaLink = data.data.children[i].data.permalink;
+    let link = "http://reddit.com" + permaLink;
     let image = data.data.children[i].data.thumbnail;
-    box.innerHTML = `<br>name: ${name}<br> Title: ${titler}<br> Link: ${link}<br> image: ${image}<br>`;
+    // box.innerHTML = `<br>name: ${name}<br> Title: ${titler}<br> Link: ${link}<br> image: ${image}<br>`;
+    let userName = document.createElement("h4");
+    let theTitle = document.createElement("h1");
+    let theImage = document.createElement("img");
+    theImage.setAttribute("src", image)
+    let theLink = document.createElement("a");
+    theLink.setAttribute("href", link)
+    userName.append(name);
+    theTitle.append(titler);
+    theImage.append(image);
+    theLink.append(link);
+    box.appendChild(userName);
+    box.appendChild(theTitle);
+    box.appendChild(theImage);
+    box.appendChild(theLink);
     body.appendChild(box);
     }
+}).catch(err => {
+    console.log("the error is: ", err)
 })
